ed854b8ffb66068c390bf5ad43d151d58930a822
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-06-14 14:32:05 -0700

    Add get page to dictionary

--- models/dictionary.py
+++ models/dictionary.py
@@ -3,8 +3,12 @@ from .page import Page
 class Dictionary:
 
     def __init__(self, words: list[str]) -> None:
-        self.pages = []
+        self.pages: list[Page] = []
         for i in range(len(words), Page.MAX_WORDS):
             page_words = words[i: min(i + Page.MAX_WORDS, len(words))]
             page = Page(page_words)
             self.pages.append(page)
+
+    def get_page(self, page_number: int) -> list[str]:
+        assert 0 <= page_number < len(self.pages)
+        return self.pages[page_number - 1].words
